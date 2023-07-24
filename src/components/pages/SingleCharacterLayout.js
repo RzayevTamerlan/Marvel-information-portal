b9e1d876096fdc36a847d66dc8814995baf4c5c6
rzayevtamerlan <turgay.rzayev@gmail.com>
2023-07-24 14:18:34 +0400

    Added React Helmet. Now every page has his own title and description.

--- src/components/pages/SingleCharacterLayout.js
+++ src/components/pages/SingleCharacterLayout.js
@@ -1,7 +1,7 @@
-
 import { useParams } from "react-router-dom";
 import useMarvelService from "../../services/MarvelService";
 import { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 import "./singleCharacterLayout.scss";
 import Spinner from "../spinner/Spinner";
@@ -21,21 +21,25 @@ const SingleCharacterLayout = () => {
   const onCharLoaded = (char) => {
     setChar(char[0]);
   };
-  console.log(char)
+  console.log(char);
   const spinner = loading ? <Spinner /> : null;
   const content = !(loading || !char) ? <View char={char}></View> : null;
-  return(
+  return (
     <>
       {spinner}
       {content}
     </>
-  )
+  );
 };
 
-const View = ({char}) => {
+const View = ({ char }) => {
   const { name, description, thumbnail } = char;
   return (
     <div className="single-char">
+      <Helmet>
+        <meta name="description" content={`${name} character page`} />
+        <title>Character | {name}</title>
+      </Helmet>
       <img src={thumbnail} alt={name} className="single-char__char-img" />
       <div className="single-char__info">
         <h2 className="single-char__name">{name}</h2>
@@ -43,6 +47,6 @@ const View = ({char}) => {
       </div>
     </div>
   );
-}
+};
 
 export default SingleCharacterLayout;
--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -2,7 +2,7 @@ import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
-import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Helmet } from "react-helmet";
 import "./singleComicPage.scss";
 
 const SingleComicPage = () => {
@@ -36,6 +36,10 @@ const View = ({ comic }) => {
   const { title, description, pageCount, language, thumbnail, price } = comic;
   return (
     <div className="single-comic">
+      <Helmet>
+        <meta name="description" content={`${title} comic page`} />
+        <title>Comic | {title}</title>
+      </Helmet>
       <img src={thumbnail} alt={title} className="single-comic__img" />
       <div className="single-comic__info">
         <h2 className="single-comic__name">{title}</h2>
